c9049119c918d2015687cbc1891c89c3e0f4f702
priyachithiravel50 <priyachithiravel50@gmail.com>
2025-03-19 16:11:43 +0530

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EmployeeTable from './Components/EmployeeTable'; 
+import Login from './Components/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/etable" element={<EmployeeTable/>} /> 
       </Routes>
     </Router>
