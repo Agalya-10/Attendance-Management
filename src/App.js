394b8309335a3e92f1cd86088584ae71814549f0
Agalya-10 <aagalya360@gmail.com>
2025-03-24 12:53:28 +0530

    table

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EmployeeTable from './Components/EmployeeTable'; 
 import LoginForm from './Components/Login';
 import Attendance from "./Components/Attendance";
+import Settings from "./Components/Settings";
+
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Route path="/" element={<LoginForm/>} /> 
         <Route path="/etable" element={<EmployeeTable/>} /> 
         <Route path="/timer" element={<Attendance/>} /> 
+        <Route path="/settings" element={<Settings/>} /> 
+
       </Routes>
     </Router>
   );
