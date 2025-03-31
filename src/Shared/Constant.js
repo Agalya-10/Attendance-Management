229cc89118c0e3279755d2339ebe04fddc190009
Agalya-10 <aagalya360@gmail.com>
2025-03-31 20:21:14 +0530

    Table

--- src/Shared/Constant.js
+++ src/Shared/Constant.js
@@ -9,45 +9,24 @@ export const ROUTES = {
   successPage: '/dashboard',
 };
 
-export const COMPONENT_LABEL = {
-  LABEL_DASHBOARD: 'Dashboard',
-  LABEL_EMPLOYEES: 'Employee Table',
-  LABEL_LEAVES: 'Leave Table',
-  LABEL_ATTENDANCE: 'Attendance Page',  
-  LABEL_ATTENDANCEREPORT: 'Attendance Report',
-  LABEL_SETTINGS: 'Settings',
-}
-
-
-
-export const employees = [
-  { id: 1, name: "Bavya", department: "Frontend Developer" },
-  { id: 2, name: "DhivyaBharathi", department: "Backend Developer" },
-  { id: 3, name: "Rajapriya", department: "Frontend Developer" },
-  { id: 4, name: "Keerthana", department: "Frontend Developer" },
-  { id: 5, name: "Prakash", department: "Frontend Developer" },
-  { id: 6, name: "Tamilselvan", department: "Backend Developer" },
-  { id: 7, name: "Vanmathi", department: "Backend Developer" },
-  { id: 8, name: "Vinothini", department: "Frontend Developer" },
-  { id: 9, name: "Venkat Rentala", department: "Frontend Developer" },
-  { id: 10, name: "Agalya", department: "Frontend Developer" },
-  { id: 11, name: "Amsavarthani", department: "Backend Developer" },
-  { id: 12, name: "Priya", department: "Frontend Developer" },
-  { id: 13, name: "Pavithra", department: "Frontend Developer" },
-  { id: 14, name: "Gowthamraj", department: "Backend Developer" },
-  { id: 15, name: "Minar Vengat", department: "Frontend Developer" },
-  { id: 16, name: "Kanimozhi", department: "Frontend Developer" },
-  { id: 17, name: "Parthiban", department: "Frontend Developer" },
-  { id: 18, name: "Tamil Nila", department: "Backend Developer" },
-  { id: 19, name: "Dhayanithi", department: "Backend Developer" },
+export const defaultEmployees = [
+  { id: 1, name: "Bavya", dob: "2003-05-12", department: "Frontend Developer" },
+  { id: 2, name: "DhivyaBharathi", dob: "2002-11-14", department: "Backend Developer" },
+  { id: 3, name: "Rajapriya", dob: "2002-12-14", department: "Frontend Developer" },
+  { id: 4, name: "Keerthana", dob: "2002-04-06", department: "Frontend Developer" },
+  { id: 5, name: "Prakash", dob: "2000-07-06", department: "Frontend Developer" },
+  { id: 6, name: "Tamilselvan", dob: "1995-06-15", department: "Backend Developer" },
+  { id: 7, name: "Vanmathi", dob: "2000-08-13", department: "Backend Developer" },
+  { id: 8, name: "Vinothini", dob: "2002-12-18", department: "Frontend Developer" },
+  { id: 9, name: "Venkat Rentala", dob: "1995-06-15", department: "Frontend Developer" },
+  { id: 10, name: "Agalya", dob: "2004-05-10", department: "Frontend Developer" },
+  { id: 11, name: "Amsavarthani", dob: "2003-08-18", department: "Backend Developer" },
+  { id: 12, name: "Priya", dob: "2003-11-26", department: "Frontend Developer" },
+  { id: 13, name: "Pavithra", dob: "2003-09-26", department: "Frontend Developer" },
+  { id: 14, name: "Gowthamraj", dob: "2001-06-20", department: "Backend Developer" },
+  { id: 15, name: "Minar Vengat", dob: "2005-01-17", department: "Frontend Developer" },
+  { id: 16, name: "Kanimozhi", dob: "2003-07-21", department: "Frontend Developer" },
+  { id: 17, name: "Parthiban", dob: "2003-01-29", department: "Frontend Developer" },
+  { id: 18, name: "Tamil nila", dob: "1996-06-05", department: "Backend Developer" },
+  { id: 19, name: "Dhayanithi", dob: "1995-06-15", department: "Backend Developer" },
 ];
-
-export const tableHeaders = [
-  { id: "sno", label: "S No" },
-  { id: "name", label: "Employee Name" },
-  { id: "department", label: "Department" },
-  { id: "status", label: "Status" },
-  { id: "action", label: "Action" }
-];
-
-export const PRIMARY_COLOR = "#EC155B";
